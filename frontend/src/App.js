a12de3bd6a7aac42b3c3c68f40f30a412f927cdc
pushpraj15295 <101568029+pushpraj15295@users.noreply.github.com>
2022-11-12 18:09:37 +0530

    complete app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,25 @@
 import { Center } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
 import Navebar from "./components/Navbar";
-import Contact from "./Pages/Contact";
 import Home from "./Pages/Home";
-import Login from "./Pages/Login";
-import Signup from "./Pages/Signup";
 import AllRoutes from "./Router/AllRoutes";
-import About from "./Pages/About";
-import Bottombar from "./components/Bottombar";
-import Checkout from "./Pages/Checkout";
+
+
 
 function App() {
   return (
-       <Checkout/>
-    // <div className="App">
-    //   <Center>
-    //     <Navebar />
-    //   </Center>
-    //   {/* <div><Link to="/login">Login</Link>
-    //    <Link to="/signup">signup</Link>
-    //    <Link to="/forgetpassword">ForgetPassword</Link>
-    //    <Link to="/resetPassword">ResetPassword</Link>
-    //    </div> */}
-    //      <AllRoutes/>
-    //   <Home />
-    //   {/* <Contact /> */}
-    //   {/* <About /> */}
+    <div className="App">
+      <Center>
+        <Navebar />
+      </Center>
+      <Home />
 
-    //   <AllRoutes />
-    //   <Center>
-    //     <Bottombar />
-    //   </Center>
-    //   <Center bg="#1f1f1f" color="white">
-    //     <Footer bg="#1f1f1f" />
-    //   </Center>
-    // </div>
+      <Center bg="#1f1f1f" color="white">
+        <Footer bg="#1f1f1f" />
+      </Center>
+      <AllRoutes />
+    </div>
   );
 }
 
