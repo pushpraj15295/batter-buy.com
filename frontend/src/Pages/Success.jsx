ddd9fe44455471b7160dc6820e525d6d7ec750a8
arsalanshaikhh <arsalan4coder@gmail.com>
2022-11-12 23:52:44 +0530

    success page

---
+++ frontend/src/Pages/Success.jsx
@@ -0,0 +1,60 @@
+import { Box, Button, Center, Flex, Image, Text } from "@chakra-ui/react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Success = () => {
+  const navigate = useNavigate();
+
+  return (
+    // <div style={{ margin: "auto" }}>
+    <Box
+      maxW={"6xl"}
+      justify="space-between"
+      wrap="wrap"
+      m="auto"
+      marginBottom="50px"
+      marginTop="50px"
+    >
+      <Box w={"1xl"}>
+        <Center>
+          <Text fontSize="32px" fontWeight="500" lineHeight="48px" color="gray">
+            Thank You For Shopping.
+          </Text>
+        </Center>
+        <Center>
+          <Image
+            maxW="600px"
+            src="https://cdn.dribbble.com/users/1751799/screenshots/5512482/check02.gif"
+          />
+        </Center>
+
+        <Center>
+          <Text fontSize="32px" fontWeight="500" lineHeight="48px" color="gray">
+            Your Order Has Been Placed.
+          </Text>
+        </Center>
+        <Center>
+          {" "}
+          <Button
+            color="white"
+            bg="black"
+            p="12px 24px"
+            fontSize="13px"
+            borderRadius="3px"
+            marginTop="20px"
+            // onClick={() => {
+            //   setTimeout(() => navigate("/"), 4000);
+            // }}
+
+            onClick={() => navigate("/")}
+          >
+            Go To Home
+          </Button>
+        </Center>
+      </Box>
+    </Box>
+    // </div>
+  );
+};
+
+export default Success;
--- frontend/src/Router/AllRoutes.jsx
+++ frontend/src/Router/AllRoutes.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Cart from '../Pages/Cart'
-import ForgetPassword from '../Pages/ForgetPassword'
-import Home from '../Pages/Home'
-import Login from '../Pages/Login'
-import ResetPassword from '../Pages/ResetPassword'
-import Signup from '../Pages/Signup'
-import Wishlist from '../Pages/Wishlist'
-import Allproducts from "../Pages/Allproducts"
-import {Description} from "../Pages/Description"
-import About from "../Pages/About"
-import Contact from "../Pages/Contact"
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Cart from "../Pages/Cart";
+import ForgetPassword from "../Pages/ForgetPassword";
+import Home from "../Pages/Home";
+import Login from "../Pages/Login";
+import ResetPassword from "../Pages/ResetPassword";
+import Signup from "../Pages/Signup";
+import Wishlist from "../Pages/Wishlist";
+import Allproducts from "../Pages/Allproducts";
+import { Description } from "../Pages/Description";
+import About from "../Pages/About";
+import Contact from "../Pages/Contact";
+import Success from '../Pages/Success'
 const AllRoutes = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/aboutus" element={<About/>} />
-        <Route path="/contactus" element={<Contact/>} />
+        <Route path="/aboutus" element={<About />} />
+        <Route path="/contactus" element={<Contact />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/forgetpassword" element={<ForgetPassword />} />
         <Route path="/resetPassword" element={<ResetPassword />} />
@@ -27,8 +28,9 @@ const AllRoutes = () => {
         <Route path="/women" element={<Allproducts />} />
         <Route path="/kids" element={<Allproducts />} />
         <Route path="/description" element={<Description />} />
-        <Route path="/wishlist" element={<Wishlist/>}/>
-        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/success" element={<Success />} />
       </Routes>
     </>
   );
